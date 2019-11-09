475e12e26e78e3448db05613281c3d17ca1a9fca
Arren De Manuel <ademanue@ucsd.edu>
2019-11-08 17:44:45 -0800

    First commit

---
+++ Assets/Scripts/Brick/BrickBreaker.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//The point of this script is to turn on kinematics on cubes that were clicked on
+//So the way this works is that when the player clicks on the brick, it creates a raycast that finds the point of contact from the camera to the brick
+
+public class BrickBreaker : MonoBehaviour
+{
+    [SerializeField] LayerMask clickMask;
+    [SerializeField] float radius = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Vector3 clickPosition = -Vector3.one;   //Just a default value
+
+            //Raycasting using colliders
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit, 100f, clickMask))
+            {
+                clickPosition = hit.point;
+
+                //Now create a sphere collider here grab all the cubes there
+                Collider[] colliders = Physics.OverlapSphere(clickPosition, radius, clickMask);
+
+                //Play sound
+                GetComponent<AudioSource>().Play();
+                foreach(Collider collider in colliders)
+                {
+                    collider.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+                    //Push this dude
+                    collider.gameObject.GetComponent<Rigidbody>().AddExplosionForce(500.0f, clickPosition, 500.0f);
+                    collider.transform.parent = null;
+                    collider.gameObject.layer = 10;
+                }
+                
+            }
+
+
+
+            Debug.Log(clickPosition);
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/Brick/BrickMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script controls the movement of the brick mainly through the rotations of it
+public class BrickMovement : MonoBehaviour
+{   
+
+    Vector2 startingTapPos = new Vector2();
+    Vector2 currentTapPos = new Vector2();
+    Vector2 differenceInPos = new Vector2();
+
+    Vector3 axisTowards = new Vector3();
+    Vector3 axisUpwards = new Vector3();
+
+    [SerializeField] float speed = 5.0f;
+
+    GameObject cam;
+
+    bool mouseHeld = false;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        cam = FindObjectOfType<Camera>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mouseHeld)
+        {
+            
+        }
+
+    }
+
+    //When the mouse is pressed, save the starting position of the mouse
+    private void OnMouseDown()
+    {
+        mouseHeld = true;
+        startingTapPos = Input.mousePosition;
+        
+    }
+    
+    private void OnMouseUp()
+    {
+        mouseHeld = false;
+    }
+    //When the mouse is being dragged
+    private void OnMouseDrag()
+    {
+        currentTapPos = Input.mousePosition;
+        differenceInPos = startingTapPos - currentTapPos;
+        axisTowards = cam.transform.position - transform.position;
+        axisUpwards = new Vector3(axisTowards.x + 90.0f, axisTowards.y, axisTowards.z);
+
+        transform.Rotate(Vector3.up, differenceInPos.x * speed);
+        startingTapPos = currentTapPos;
+    }
+}
